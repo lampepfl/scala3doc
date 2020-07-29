9e71c000dd13ef3842d6d13789c1ab29ea1b0427
Krzysztof Romanowski <romanowski.kr@gmail.com>
2020-07-29 03:03:50 +0200

    Add missing bits in signatures
    Render objects, traits and classes.
    Add support for variance and var/val.
    Refactor tests for easier development.

--- src/main/scala/dotty/dokka/tasty/TastyParser.scala
+++ src/main/scala/dotty/dokka/tasty/TastyParser.scala
@@ -24,7 +24,7 @@ import dotty.tastydoc.representations
 import dotty.tastydoc.representations._
 
 case class TastyParser(reflect: Reflection, inspector: DokkaTastyInspector) 
-  extends ScaladocSupport with BasicSupport with TypesSupport with ClassLikeSupport:
+  extends ScaladocSupport with BasicSupport with TypesSupport with ClassLikeSupport with SyntheticsSupport:
     import reflect._
 
     def sourceSet = inspector.sourceSet
@@ -36,7 +36,6 @@ case class TastyParser(reflect: Reflection, inspector: DokkaTastyInspector)
           tree match {
             case clazz: ClassDef  =>
               docs += parseClass(clazz)
-              //classes += DDClass(clazz.name, "dotty.dokka", clazz.symbol.comment.fold("")(_.raw))  
             case _ =>
           }
           super.traverseTree(tree)
