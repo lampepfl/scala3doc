1b7da91559fca2860a5a149b801f101bdaf17173
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@epfl.ch>
2020-10-19 20:34:16 +0200

    Fix all warnings

--- src/main/scala/dotty/dokka/transformers/ScalaSourceLinksTransformer.scala
+++ src/main/scala/dotty/dokka/transformers/ScalaSourceLinksTransformer.scala
@@ -34,7 +34,8 @@ class ScalaSourceLinksTransformer(
 
     override def invoke(input: DModule, context: DokkaContext): DModule = 
         input.updateMembers { 
-            case c: Member with WithExpectActual with WithExtraProperties[Member] => 
+            case c0: (Member & WithExpectActual & WithExtraProperties[_]) => 
+                val c = c0.asInstanceOf[Member & WithExpectActual & WithExtraProperties[Member]]
                 c.withNewExtras(c.getExtra plus getSourceLinks(c))
             case c => c
         }
