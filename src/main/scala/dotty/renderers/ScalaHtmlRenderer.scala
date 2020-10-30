427e6bb14d9dd0aefa94a2cfec591e6c5f8b0d9e
Guillaume Raffin <theelectronwill@gmail.com>
2020-10-30 23:09:30 +0100

    Remove paneSearch

--- src/main/scala/dotty/renderers/ScalaHtmlRenderer.scala
+++ src/main/scala/dotty/renderers/ScalaHtmlRenderer.scala
@@ -242,7 +242,6 @@ class ScalaHtmlRenderer(ctx: DokkaContext) extends SiteRenderer(ctx) {
                         div(
                             id := "leftColumn",
                             div(id := "logo"),
-                            div(id := "paneSearch"),
                             nav(id := "sideMenu"),
                         ),
                         div(
